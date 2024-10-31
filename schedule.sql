f5126800e7af17ab38b8fa5b86bcb8e6e52918d1
kimdanbin <rlaeksqls@naver.com>
2024-10-31 17:59:12 +0900

    [sql]: lv0 SQL 수정

--- schedule.sql
+++ schedule.sql
@@ -1,33 +1,33 @@
 -- user 테이블
-CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(10) NOT NULL
-);
+-- CREATE TABLE users (
+--     user_id INT PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(10) NOT NULL
+-- );
 
 -- schedule 테이블
 CREATE TABLE schedules (
     schedule_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    writer VARCHAR(45) NOT NULL,
     todo VARCHAR(45) NOT NULL,
     password INT,
     last_update DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES uswer(user_id)
+--     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 -- user 생성 query
-INSERT INTO users (user_id, name) VALUES
-(1, 'kim');
+-- INSERT INTO users (user_id, name) VALUES
+-- (1, 'kim');
 
 -- 일정 생성 query
-INSERT INTO schedules (schedule_id, user_id, todo, password, "last_update") VALUES
-(1, 1, '공부하기', 1234, '2024-10-31 17:18:00');
+INSERT INTO schedules (schedule_id, writer, todo, password, "last_update") VALUES
+(1, 'kim', '공부하기', 1234, '2024-10-31 17:18:00');
 
 -- 전체 일정 조회 query
-SELECT schedule_id, user_id, todo, last_update
+SELECT schedule_id, writer, todo, last_update
 FROM schedules;
 
 -- 선택 일정 조회 query
-SELECT schedule_id, user_id, todo, last_update
+SELECT schedule_id, writer, todo, last_update
 FROM schedules
 WHERE schedule_id = 1;
 
